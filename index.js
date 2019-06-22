69e81d7db2b44fc954184b5003611e4903a24252
eun-kyoung <dossiba9603@naver.com>
2019-06-22 21:24:39 +0900

    middleware and delay 2sec

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const PORT = 4000;
 const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
 const handleHome = (req,res) => res.send("response!!");
 const handleProfile = (req,res) => res.send("You are on my profile");
+const betweenHome = (req,res,next) => {
+    console.log("Between");
+    next();
+}
 
-app.get("/", handleHome);
+app.use(betweenHome);
+app.get("/", betweenHome, handleHome);
 app.get("/profile", handleProfile);
 
 app.listen(4000, handleListening);

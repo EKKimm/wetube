a84e0c9e8f6fdea1e44bad57274d821c6de50a31
eun-kyoung <dossiba9603@naver.com>
2019-06-22 18:32:19 +0900

    babel을 이용하여 arrow function사용, nodemon을 이용하여 저장할 때 마다 서버 새로 시작함(중지할 필요 없음)

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`Listening on : http://localhost:${PORT}`);
-}
-
-function handleHome(req,res){
-    console.log(req);
-    res.send("response!!");
-}
-
-function handleProfile(req,res){
-    res.send("You are on my profile");
-}
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+const handleHome = (req,res) => res.send("response!!");
+const handleProfile = (req,res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
 
 app.listen(4000, handleListening);
